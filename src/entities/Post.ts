98a939dbd2376de861bacc417a5143459f82d1a6
rsingh25 <raghvendra.pratap.singh@gmail.com>
2021-02-19 07:27:50 +0530

    Added express, graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 export class Post {
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./contants";
-import { Post } from "./entities/Post"
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolves/post";
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroConfig);
-    await orm.getMigrator().up();
-    
-    const post = orm.em.create(Post, {title: 'my 3 post'})
-    await orm.em.persistAndFlush(post);
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
-    console.log(await orm.em.find(Post, {}));
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Started server on port 4000");
+  });
+
 };
 
-main().catch( (err) => {
-    console.error(err)
+main().catch((err) => {
+  console.error(err);
 });
-
